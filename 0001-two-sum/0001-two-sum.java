30ccee1fc68f00c2c36037672c2d74a8f85d8663
Abhishek Pathak <pathakabhi290@gmail.com>
2026-08-26 22:56:34 +0530

    Time: 2 ms (99.34%), Space: 47.4 MB (11.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,37 +1,60 @@
+// Simple another approach 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-         int il =0;
-          int jl =0;
-        for(int i=0; i< n; i++){
-            for(int j =i+1; j< n; j++){
-                if(target == nums[i]+ nums[j]){
-il =i;
-jl =j;
-break;
-                }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int needed = target - nums[i];
+
+            if (map.containsKey(needed)) {
+                return new int[]{map.get(needed), i};
             }
+
+            map.put(nums[i], i);
         }
-        // int i=0;
-        // int j=0;
-        // int l =0;
-        
-        //  int r = nums.length-1;
-         
-        //  while(l < r){
-        //     int val = nums[l] + nums[r];
-        //     if(target == val){
-        //         i=l;
-        //         j=r;
-        //         break;
-        //     }
-        //    else if(val < target){
-        //         l++;
-        //     }
-        //     else{
-        //         r--;
-        //     }
-        //  }
-         return new int[]{il,jl};
+
+        return new int[]{};
     }
 }
+
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int n = nums.length;
+//          int il =0;
+//           int jl =0;
+//         for(int i=0; i< n; i++){
+//             for(int j =i+1; j< n; j++){
+//                 if(target == nums[i]+ nums[j]){
+// il =i;
+// jl =j;
+// break;
+//                 }
+//             }
+//         }
+//         // int i=0;
+//         // int j=0;
+//         // int l =0;
+        
+//         //  int r = nums.length-1;
+         
+//         //  while(l < r){
+//         //     int val = nums[l] + nums[r];
+//         //     if(target == val){
+//         //         i=l;
+//         //         j=r;
+//         //         break;
+//         //     }
+//         //    else if(val < target){
+//         //         l++;
+//         //     }
+//         //     else{
+//         //         r--;
+//         //     }
+//         //  }
+//          return new int[]{il,jl};
+//     }
+// }
+
